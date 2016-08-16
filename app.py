004ed11266be59546b515c175d0b48f9f0c6d379
Thiago Araujo <worf.mogh@gmail.com>
2016-08-16 17:29:53 -0300

    convert xml to object (dict) and save it on mongodb

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+from __future__ import print_function
 import os
+import sys
+import xmltodict, json
 from flask import Flask, redirect, url_for, request, render_template
 from pymongo import MongoClient
 
@@ -7,16 +10,16 @@ app = Flask(__name__)
 client = MongoClient(
     os.environ['DB_PORT_27017_TCP_ADDR'],
     27017)
-db = client.tododb
+db = client.lattes
 
 
 @app.route('/')
-def todo():
+def lattes():
 
-    _items = db.tododb.find()
+    _items = db.lattes.find()
     items = [item for item in _items]
 
-    return render_template('todo.html', items=items)
+    return render_template('lattes.html', items=items)
 
 
 @app.route('/new', methods=['POST'])
@@ -26,9 +29,24 @@ def new():
         'name': request.form['name'],
         'description': request.form['description']
     }
-    db.tododb.insert_one(item_doc)
+    #TODO no uploads
+    #db.lattes.insert_one(item_doc)
 
-    return redirect(url_for('todo'))
+    return redirect(url_for('lattes'))
+
+@app.route('/load', methods=['POST'])
+def load():
+    file_path = request.form['path']
+    print(file_path, file=sys.stderr)
+    print('Hello world!', file=sys.stderr)
+
+    with open(file_path) as file:
+        data = file.read()
+        obj = xmltodict.parse(data)
+        db.lattes.insert_one(obj)
+
+    return redirect(url_for('lattes'))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
