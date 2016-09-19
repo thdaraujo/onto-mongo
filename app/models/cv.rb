9387ac74079afce436e5ff975ca219972f7fd3c2
Thiago Araujo <worf.mogh@gmail.com>
2016-09-19 16:11:57 -0300

    basic map and filtering

--- app/models/cv.rb
+++ app/models/cv.rb
@@ -7,13 +7,24 @@ class CV
     self.CURRICULO_VITAE["DADOS_GERAIS"]["NOME_COMPLETO"]
   end
 
+  def email
+    self.CURRICULO_VITAE["DADOS_GERAIS"]["NOME_COMPLETO"]
+  end
+
   #TODO mudar para uma classe de configuracao, estilo FactoryGirl.
   # acho que fica melhor e mais claro
   # pois uma mesma model/doc do mongo pode ter várias classes relacionadas na ontologia.
   def setup
     ontoclass 'foaf:Person'
-    maps :name, to: 'foaf:name'
+    maps('foaf:name', :name)
+    maps('foaf:mbox', :email)
     print
+
+    cvs = query("teste")
+    puts ">>>>>>>>>>>>>>>>>>>>> query('teste')"
+    puts ''
+    puts "query('teste')"
+    p cvs
   end
 
 end
--- lib/onto_map.rb
+++ lib/onto_map.rb
@@ -1,31 +1,94 @@
 # https://robots.thoughtbot.com/writing-a-domain-specific-language-in-ruby
 
 module OntoMap
-  @registry = {}
+  attr_accessor :mapping, :ontology_class
+
+  @mapping = {}
   @ontology_class = nil
+
+=begin
+
+  @attributes = {}
+
+  def method_missing(name, *args)
+    attribute = name.to_s
+    if attribute =~ /=$/
+      @attributes[attribute.chop] = args[0]
+    elsif @attributes[attribute].present?
+      @attributes[attribute]
+    else
+      self.send @mapping[attribute]
+    else
+
+    end
+    puts attribute
+  end
+=end
 
   def ontoclass(ontology_class)
     @ontology_class = ontology_class
   end
 
-  def maps(property, relation)
-    subject = @ontology_class
-    @registry[relation] = property
-
-    puts @registry
+  def maps(relation, property)
+    @mapping ||= {}
+    @mapping[relation] = property
   end
 
   def query(sparql)
-    #TODO
-    map = @registry.map{|rel, filter| filter.call }
-    # ? reduce = map.inject{|| }
-    puts sparql
-    puts map
+    expanded_query = expand_query(sparql)
+    query = build_query(expanded_query)
+    model = nil
+    result = []
+
+    # descobrir qual é a model: quem for da ontoclass foaf:Person
+    # vamos supor que seja sempre CV no momento (TODO)
+    if query[:ontoclass] == 'foaf:Person'
+      model = CV
+      # TODO filter...
+      result = model.all
+    end
+
+    query[:relations].each do |relation|
+      # where :name == fulano, etc.
+      prop = relation[:property]
+      property = @mapping[prop]
+      value = relation[:object]
+
+      puts "prop = #{prop}, #{property} == #{value}"
+
+
+      # TODO filter...
+      result = result.to_a.select{|cv|
+        cv.send(property) == value
+      }
+    end
+    result
+  end
+
+  def expand_query(sparql)
+    # TODO montar uma estrutura +- como essa a partir do sparql
+    # exemplo:
+    [{ subject: "?person", property: :a, object: 'foaf:Person'     },
+     { subject: "?person", property: "foaf:name", object: 'Wagner da Silva' },
+     { subject: "?person", property: "foaf:mbox", object: 'teste@gmail.com' }]
+  end
+
+  def build_query(arr)
+    # TODO
+    # query.inject  ... montar um hash de subjects com suas listas de relações/properties
+    # por exemplo:
+    # ?person -> [a: "foaf:Person", "foaf:name" = "fulano", "foaf:mbox" = "123@4"]
+    # pode ser um objeto tbm... fica mais facil.
+
+    {
+      subject: '?person',
+      ontoclass: 'foaf:Person',
+      relations: [{ property: "foaf:name", object: 'Wagner da Silva' }]
+    }
   end
 
   def print
     puts "Ontoclass: #{@ontology_class}"
-    puts "Registry:"
-    puts @registry
+    puts "Mapping: #{@mapping.to_s}"
   end
 end
