09e8a1265bdffc0e78f741cae3e1ef38900521f7
BarbaraTieko <barbara_tieko@hotmail.com>
2016-10-13 00:53:01 -0300

    Fix OntoSplit

--- lib/onto_split.rb
+++ lib/onto_split.rb
@@ -1,17 +1,19 @@
+require 'sparql'
 module OntoSplit
   def self.split(sparql)
-    where = sparql.downcase.split("where")
-
-    w = where[1].delete '{' '}'
-    triples = w.split(/[.;\n]/)
-
     t_arr = Array.new
-    triples.each do |t|
-      if !(t.empty?) || t.length > 3
-        t_arr << Triple.new(t.split)
+    sse = SPARQL.parse(sparql).to_sse
+
+    sse.each_line do |line|
+      if line.include? "triple"
+        line = line.delete '(' ')'
+        line = line.gsub('triple','')
+        triple = line.split(/\s(?=(?:[^"]|"[^"]*")*$)/)
+
+        t_arr << Triple.new(triple.reject { |c| c.empty? })
       end
     end
-
     return t_arr
   end
+
 end
