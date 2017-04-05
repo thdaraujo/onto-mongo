3b691020c7cd03f333768149ba19039046ea67e0
Thiago Araujo <thd.araujo@gmail.com>
2017-04-05 15:42:11 -0300

    adjusting mapping (WIP)

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -14,15 +14,15 @@ class Publication
   field :doi, type: String
   field :coauthors, type: Array
 
-  ontoclass 'foaf:Publication'
-  maps from: ':nature', to:  :nature
-  maps from: ':title', to:  :title
-  maps from: ':title_en', to:  :title_en
-  maps from: ':year', to:  :year
-  maps from: ':country', to:  :country
-  maps from: ':language', to:  :language
-  maps from: ':medium', to:  :medium
-  maps from: ':doi', to:  :doi
+  #ontoclass 'foaf:Publication'
+  #maps from: ':nature', to:  :nature
+  #maps from: ':title', to:  :title
+  #maps from: ':title_en', to:  :title_en
+  #maps from: ':year', to:  :year
+  #maps from: ':country', to:  :country
+  #maps from: ':language', to:  :language
+  #maps from: ':medium', to:  :medium
+  #maps from: ':doi', to:  :doi
 
   def self.from_hash(hash)
     publications = []
